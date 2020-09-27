64ef6aa8a66f8cb4994357f9a979d0b36cd18052
hector036 <hector.achi.15@gmail.com>
2020-09-27 20:48:39 +0600

    Two sum

---
+++ Array/1. Two sum/two_sum.cpp
@@ -0,0 +1,16 @@
+// Time Comx : O(n)
+// Space Comx : O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        for(int i = 0;i<nums.size();i++){
+            int complement = target - nums[i];
+            if(mp.count(complement)){
+                return {mp[complement],i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
